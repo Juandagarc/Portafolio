6c727ded5d0f6216a39e6246a87c5b168991a23a
juanda <juandagarce@gmail.com>
2023-07-21 23:55:50 -0500

    First idea

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Background from './components/background/background'
+
+function App() {
+
+  return (
+    <>
+      <Background />
+    </>
+  )
+}
+
+export default App
