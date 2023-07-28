96dd5b12006511a12a2ccf63203fd7e26db26bf8
Juandagarc <juandagarce@gmail.com>
2023-07-28 13:44:58 -0500

    Finalizado

--- frontend/src/Containers/about-container/about-container.tsx
+++ frontend/src/Containers/about-container/about-container.tsx
@@ -1,25 +1,22 @@
-import About from "../../components/about/about.tsx"
-import TitleAbout from "../../components/title-about/title-about.tsx"
-import "./about-container.css"
-
+import About from "../../components/about/about.tsx";
+import TitleAbout from "../../components/title-about/title-about.tsx";
+import "./about-container.css";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import { particlesOptions } from "./particlesConfig";
 
-const particlesInit = (engine) => {
-    loadFull(engine);
-  };
+const particlesInit = (engine: any) => {
+  return Promise.resolve(loadFull(engine));
+};
 
 const AboutContainer = () => {
+  return (
+    <>
+      <TitleAbout />
+      <About />
+      <Particles init={particlesInit} options={particlesOptions} />
+    </>
+  );
+};
 
-    return (
-        <>
-            <TitleAbout />
-            <About />
-        <Particles init={particlesInit} options={particlesOptions} />
-            
-        </>
-    )
-}
-
-export default AboutContainer
+export default AboutContainer;
