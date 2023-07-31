53b951a9fba795ee8e6d5b64245bd33f83877734
Juandagarc <juandagarce@gmail.com>
2023-07-30 20:15:38 -0500

    PAGE

--- frontend/src/Containers/about-container/about-container.tsx
+++ frontend/src/Containers/about-container/about-container.tsx
@@ -3,24 +3,14 @@
 import About from "../../components/about/about.tsx";
 import TitleAbout from "../../components/title-about/title-about.tsx";
 import "./about-container.css";
-import Particles from "react-tsparticles"; // Quita <IParticlesProps> de aquí
 
-import { loadFull } from "tsparticles";
-import { particlesOptions } from "./particlesConfig";
 
-const particlesInit = (engine: any) => {
-  return Promise.resolve(loadFull(engine));
-};
 
 const AboutContainer = () => {
   return (
     <>
       <TitleAbout />
       <About />
-      <Particles // Quita <IParticlesProps> de aquí también
-        init={particlesInit}
-        options={particlesOptions}
-      />
     </>
   );
 };
