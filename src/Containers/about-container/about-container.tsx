cd6391a624f494a6976566faf53d188cbb25082d
Juandagarc <juandagarce@gmail.com>
2023-08-06 17:22:46 -0500

    video and icons

--- src/Containers/about-container/about-container.tsx
+++ src/Containers/about-container/about-container.tsx
@@ -1,19 +1,24 @@
-// Importaciones...
 
-import About from "../../components/about/about.js";
-import Cube from "../../components/cube/cube.js";
-import TitleAbout from "../../components/title-about/title-about.js";
-import "./about-container.css";
-
-
-
+import About from '../../components/about/about.js';
+import Cube from '../../components/cube/cube.js';
+import TitleAbout from '../../components/title-about/title-about.js';
+import Video from '../../assets/videoplayback.mp4';
+import './about-container.css';
 
 const AboutContainer = () => {
   return (
     <div className="background">
+      <div className='content-about' >
       <TitleAbout />
       <About />
       <Cube />
+      </div>
+      <video className='video' src={Video} autoPlay loop muted>
+        {/* Agrega la etiqueta <source> para manejar diferentes formatos de video */}
+        <source src={Video} type="video/mp4" />
+        {/* Agrega más elementos <source> para otros formatos de video como WebM o Ogg */}
+        Tu navegador no admite el elemento de video.
+      </video>
     </div>
   );
 };
